96d67221b5e298067f31c3f5a2aa161389fac5ed
Ovie Tennyson <atomictenny@gmail.com>
2021-10-10 19:12:39 +0000

    moved dependies to ap.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import Index from './components/pages'
+import './index.css';
+import './components/css/reset.css';
+import './components/css/bootstrap-grid.min.css';
+import './components/css/animations.css';
+import './components/css/perfect-scrollbar.css';
+import './components/css/owl.carousel.css';
+import './components/css/magnific-popup.css';
+import './components/css/main.css';
+import './components/js/animating.js'
+import './components/js/imagesloaded.pkgd.min.js'
+import './components/js/jquery-2.1.3.min.js'
+import './components/js/jquery.googlemap.js'
+import './components/js/jquery.magnific-popup.min.js'
+import './components/js/jquery.shuffle.min.js'
+import './components/js/main.js'
+import './components/js/masonry.pkgd.min.js'
+import './components/js/modernizr.custom.js'
+import './components/js/owl.carousel.min.js'
+import './components/js/perfect-scrollbar.min.js'
+import './components/js/validator.js'
 
 
 function App() {
