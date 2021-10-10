b45574fa2ac73e3cb336f5360ac26b49a3e103f6
Ovie Tennyson <atomictenny@gmail.com>
2021-10-10 17:59:07 +0000

    Added Index.jsx and dependencies

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './component/css/reset.css';
-import './component/css/bootstrapp-grid.min.css';
-import './component/css/animations.css';
-import './component/css/perfect-scrollbar.css';
-import './component/css/owl.carousel.css';
-import './component/css/magnific-popup.css';
-import './component/css/main.css';
+import './components/css/reset.css';
+import './components/css/bootstrapp-grid.min.css';
+import './components/css/animations.css';
+import './components/css/perfect-scrollbar.css';
+import './components/css/owl.carousel.css';
+import './components/css/magnific-popup.css';
+import './components/css/main.css';
 
-import './component/js/animating.js'
-import './component/js/imagesloaded.pkgd.min.js'
-import './component/js/jquery-2.1.3.min.js'
-import './component/js/jquery.googlemap.js'
-import './component/js/jquery.magnific-popup.min.js'
-import './component/js/jquery.shittle.min.js'
-import './component/js/main.js'
-import './component/js/masonry.pkgd.min.js'
-import './component/js/modernizr.custom.js'
-import './component/js/owl.carousel.min.js'
-import './component/js/perfect-scrollbar.min.js'
-import './component/js/validator.js'
+import './components/js/animating.js'
+import './components/js/imagesloaded.pkgd.min.js'
+import './components/js/jquery-2.1.3.min.js'
+import './components/js/jquery.googlemap.js'
+import './components/js/jquery.magnific-popup.min.js'
+import './components/js/jquery.shittle.min.js'
+import './components/js/main.js'
+import './components/js/masonry.pkgd.min.js'
+import './components/js/modernizr.custom.js'
+import './components/js/owl.carousel.min.js'
+import './components/js/perfect-scrollbar.min.js'
+import './components/js/validator.js'
 
 import App from './App';
 
